e13d00e8e4933067c0f7d8380f62085410ebbd7d
Lara <sabatinilara@gmail.com>
2023-01-09 10:45:53 -0300

    test: added test for date helpers

--- src/helpers/dates/addMonths.ts
+++ src/helpers/dates/addMonths.ts
@@ -1,12 +1,16 @@
+import formatDate from "./DDMMYYYYFormat"
+
 const addMonths = (months: number, date = new Date()): string => {
   date.setMonth(date.getMonth() + months)
 
-  const day = date.getDate() > 9 ? date.getDate() : `0${date.getDate()}`
-  const month =
-    date.getMonth() + 1 > 9 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`
-  const year = date.getFullYear()
+  // const day = date.getDate() > 9 ? date.getDate() : `0${date.getDate()}`
+  // const month =
+  //   date.getMonth() + 1 > 9 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`
+  // const year = date.getFullYear()
 
-  return `${day}-${month}-${year}`
+  // return `${day}-${month}-${year}`
+
+  return formatDate(date)
 }
 
 export default addMonths
