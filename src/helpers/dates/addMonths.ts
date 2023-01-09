5db4860a79df8f302524e4a87a99d01ca1e6eeae
Lara <sabatinilara@gmail.com>
2023-01-09 10:46:24 -0300

    fix: removed comments

--- src/helpers/dates/addMonths.ts
+++ src/helpers/dates/addMonths.ts
@@ -3,13 +3,6 @@ import formatDate from "./DDMMYYYYFormat"
 const addMonths = (months: number, date = new Date()): string => {
   date.setMonth(date.getMonth() + months)
 
-  // const day = date.getDate() > 9 ? date.getDate() : `0${date.getDate()}`
-  // const month =
-  //   date.getMonth() + 1 > 9 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`
-  // const year = date.getFullYear()
-
-  // return `${day}-${month}-${year}`
-
   return formatDate(date)
 }
 
