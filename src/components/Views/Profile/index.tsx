a525b337fcb22699e5164f1e7d0b37f60d0a5a55
Lara <sabatinilara@gmail.com>
2023-01-12 11:20:34 -0300

    feat: working on client profile

--- src/components/Views/Profile/index.tsx
+++ src/components/Views/Profile/index.tsx
@@ -1,10 +1,44 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import getProfileData from "services/auth/getProfileData.service"
+import AdminInterface from "interfaces/users/Admin"
+import ClientInterface from "interfaces/users/Client"
 import Header from "components/Views/Header"
+import { useRouter } from "next/router"
+import AdminProfile from "./Admin"
+import ClientProfile from "./Client"
 
 function ProfileView() {
+  const router = useRouter()
+
+  const userData = JSON.parse(sessionStorage.getItem("userData") as string)
+
+  const [profileData, setProfileData] = useState<
+    AdminInterface | ClientInterface
+  >()
+
+  const getData = async () => {
+    const req = await getProfileData(userData.type, userData.id)
+
+    if (req.status === 200 && req.data.length) {
+      setProfileData(req.data[0])
+    } else {
+      router.replace("/404")
+    }
+  }
+
+  useEffect(() => {
+    getData()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <>
       <Header />
+      {userData.type === "admin" ? (
+        <AdminProfile data={profileData as AdminInterface} />
+      ) : (
+        <ClientProfile profile={profileData as ClientInterface} />
+      )}
     </>
   )
 }
