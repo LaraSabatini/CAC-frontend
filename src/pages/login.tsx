4c7ef4f46e36ad1e27b5d0093352dc329a7f1e99
Lara <sabatinilara@gmail.com>
2023-01-13 16:34:53 -0300

    feat: redirect in login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,20 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { useRouter } from "next/router"
 import LoginView from "components/Views/LogInView"
 
 function Login() {
+  const router = useRouter()
+
+  useEffect(() => {
+    if (Object.keys(router.query).length === 0) {
+      router.replace({
+        query: {
+          client: true,
+        },
+      })
+    }
+  }, [router])
+
   return <LoginView />
 }
 
