5b61e6353966ae088afb70e3fad6ec4ba20c6bbe
Lara <sabatinilara@gmail.com>
2023-01-16 16:08:56 -0300

    feat: created usePaymentStatus hook

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,28 +1,13 @@
-import React, { useEffect } from "react"
-import { useRouter } from "next/router"
+import React from "react"
 import userData from "const/userData"
 import useUserStatus from "hooks/isLoggedIn"
-import checkLastPayment from "helpers/dates/checkLastPayment"
+import usePaymentStatus from "hooks/usePaymentStatus"
 import DashboardView from "components/Views/Dashboard"
 
 function Dashboard() {
-  const router = useRouter()
-
   const isLoggedIn = useUserStatus(userData)
 
-  useEffect(() => {
-    if (userData.type === "client") {
-      const checkPayment = async () => {
-        const checkLastPaymentReq = await checkLastPayment(userData)
-        if (checkLastPaymentReq === "expired") {
-          router.replace("/profile")
-        }
-      }
-
-      checkPayment()
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  usePaymentStatus(userData)
 
   return <div>{isLoggedIn && <DashboardView />}</div>
 }
