03f561065d32ee6ac9bdd7305bf27e869fff9f2f
Lara <sabatinilara@gmail.com>
2023-01-23 18:10:58 -0300

    wip: articles

--- src/components/Views/Dashboard/index.tsx
+++ src/components/Views/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import Header from "components/Views/Header"
+import ArticleView from "components/Views/Artcile"
 
 function DashboardView() {
   return (
     <>
       <Header />
+      <ArticleView />
     </>
   )
 }
