bef1087a2aefc72fbb4292ccdc51125a3b9ee4f9
Lara <sabatinilara@gmail.com>
2023-01-25 21:30:54 -0300

    wip: working on file visualization

--- src/components/Views/Admin/CreateArticleForm/PrevisualizeArticle/index.tsx
+++ src/components/Views/Admin/CreateArticleForm/PrevisualizeArticle/index.tsx
@@ -2,17 +2,34 @@ import React, { useState, useContext, useEffect } from "react"
 import Button from "components/UI/Button"
 import texts from "strings/articles.json"
 import { DashboardContext } from "contexts/Dashboard"
+import { BsChevronLeft, BsChevronRight } from "react-icons/bs"
 import ArticleView from "components/Views/Articles/ArticleCard"
 import ArticleBody from "components/Views/Articles/ArticleBody"
-import { Container, ButtonContainer, Content } from "./styles"
+import {
+  Container,
+  ButtonContainer,
+  Content,
+  Previewer,
+  ArrowContainer,
+  PreviewContent,
+} from "./styles"
 
 function PrevisualizeArticle() {
-  const { newArticle, attachmentsForServer } = useContext(DashboardContext)
+  const {
+    newArticle,
+    attachmentsForServer,
+    attachmentsForDataBase,
+  } = useContext(DashboardContext)
   const [contentToShow, setContentToShow] = useState<
     "card" | "content" | "attachments"
   >("card")
 
   const [portrait, setPortrait] = useState<{ image: string }>({ image: "" })
+
+  const [docs, setDocs] = useState<any>([])
+  const [currentFile, setCurrentFile] = useState<number>(0)
+
+  const reader = new FileReader()
 
   // *** Setear imagen como URL para poder previsualizarla en la carta de articulo
   const setImageURL = () => {
@@ -20,21 +37,42 @@ function PrevisualizeArticle() {
       image => image.type.split("/")[0] === "image",
     )
 
+    const docsArray: any = []
+    for (let i = 0; i < attachmentsForServer.length; i += 1) {
+      docsArray.push({ uri: URL.createObjectURL(attachmentsForServer[i]) })
+    }
+    setDocs(docsArray)
+
     if (getImage.length) {
-      const reader = new FileReader()
-      reader.onload = () => {
-        setPortrait({ image: URL.createObjectURL(getImage[0]) })
-      }
+      setPortrait({ image: URL.createObjectURL(getImage[0]) })
       reader.readAsDataURL(getImage[0])
+    }
+  }
+
+  const goNext = () => {
+    if (currentFile + 1 < attachmentsForServer.length) {
+      setCurrentFile(currentFile + 1)
+    } else {
+      setCurrentFile(0)
+    }
+  }
+  const goPrev = () => {
+    if (currentFile - 1 > 0) {
+      setCurrentFile(currentFile - 1)
+    } else {
+      setCurrentFile(attachmentsForServer.length - 1)
     }
   }
 
   useEffect(() => {
     if (attachmentsForServer.length) {
       setImageURL()
+      setCurrentFile(0)
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [attachmentsForServer])
+
+  console.log(currentFile)
 
   return (
     <Container>
@@ -64,6 +102,39 @@ function PrevisualizeArticle() {
         )}
         {contentToShow === "content" && (
           <ArticleBody article={{ ...newArticle, author: "Flor Voglino" }} />
+        )}
+        {contentToShow === "attachments" && (
+          <Previewer>
+            {attachmentsForServer.length > 0 && (
+              <PreviewContent>
+                <ArrowContainer>
+                  <button type="button" onClick={goPrev}>
+                    <BsChevronLeft />
+                  </button>
+                  <button type="button" onClick={goNext}>
+                    <BsChevronRight />
+                  </button>
+                </ArrowContainer>
+                {attachmentsForDataBase[currentFile].type !== "image" ? (
+                  <object
+                    data={docs[currentFile].uri}
+                    type="application/pdf"
+                    width="100%"
+                    height="100%"
+                  >
+                    <p>
+                      Acceder a<a href={docs[currentFile].uri}>archivo</a>
+                    </p>
+                  </object>
+                ) : (
+                  <img
+                    alt={attachmentsForDataBase[currentFile].name}
+                    src={docs[currentFile].uri}
+                  />
+                )}
+              </PreviewContent>
+            )}
+          </Previewer>
         )}
       </Content>
     </Container>
--- src/components/Views/Admin/CreateArticleForm/PrevisualizeArticle/styles.tsx
+++ src/components/Views/Admin/CreateArticleForm/PrevisualizeArticle/styles.tsx
@@ -1,4 +1,6 @@
 import styled from "styled-components"
+import theme from "theme/index"
+import {Button} from 'components/UI/sharedStyles'
 
 const Container = styled.div``
 
@@ -7,6 +9,20 @@ const Content = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+
+  .bebe {
+    /* background-color: red; */
+    font-family: ${theme.fonts.content};
+    font-size: 14px;
+
+    .sc-iBYQkv {
+      display: none;
+    }
+
+    #arrow_left {
+      display: none;
+    }
+  }
 `
 
 const ButtonContainer = styled.div`
@@ -15,4 +31,46 @@ const ButtonContainer = styled.div`
   padding-bottom: 20px;
 `
 
-export { Container, ButtonContainer, Content }
+const Previewer = styled.div`
+  width: 700px;
+  height: 500px;
+  background-color: ${theme.colors.blue_alpha};
+  display: flex;
+  border-radius: 18px;
+`
+
+const ArrowContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  position: absolute;
+  z-index: 100;
+  top: 50%;
+  left: 2.5%;
+  width: 95%;
+
+  button {
+    ${Button}
+  }
+
+  svg {
+    width: 20px;
+    height: 20px;
+    color: ${theme.colors.white};
+  }
+`
+
+const PreviewContent = styled.div`
+  position: relative; 
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    width: 500px;
+    border-radius: 18px;
+  }
+`
+
+
+export { PreviewContent, Container, ButtonContainer, Content, Previewer, ArrowContainer, }
