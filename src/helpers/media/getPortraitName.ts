efb34b29407a2750c1a9fe7c1f4fc9f21edac39f
Lara <sabatinilara@gmail.com>
2023-05-08 13:06:27 -0300

    fix file bugs

--- src/helpers/media/getPortraitName.ts
+++ src/helpers/media/getPortraitName.ts
@@ -3,10 +3,7 @@ import renameFile from "helpers/formatting/renameFile"
 const getPortraitName = (files: FileList) => {
   const imageFileName = [...files].filter(s => s.type.includes("image"))[0].name
 
-  const imageFileExtesion = [...files].filter(s => s.type.includes("image"))[0]
-    .type
-
-  return `${renameFile(imageFileName)}.${imageFileExtesion.split("/")[1]}`
+  return `${renameFile(imageFileName)}`
 }
 
 export default getPortraitName
