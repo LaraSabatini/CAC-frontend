c467f48149157ace6c86522ca1f2a38fdee6efd8
Lara <sabatinilara@gmail.com>
2023-05-09 13:29:17 -0300

    chrome tab

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -22,6 +22,11 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Cabin:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Sora:wght@100;200;300;400;500;600;700;800&display=swap"
             rel="stylesheet"
           />
+          <link
+            rel="icon"
+            href="https://camarafederal.com.ar/software/imgs/logo-mail.png"
+          />
+          <title>Cámara Federal de Administradores</title>
         </Head>
         <body>
           <Main />
