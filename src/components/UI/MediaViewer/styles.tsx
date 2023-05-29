a727e17a4eee39523e3bff55ba5b042b1dd98fcf
Lara <sabatinilara@gmail.com>
2023-05-29 11:36:40 -0300

    fixed bugs

--- src/components/UI/MediaViewer/styles.tsx
+++ src/components/UI/MediaViewer/styles.tsx
@@ -102,7 +102,6 @@ const Content = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-
   img {
     width: 100%;
     /* height: 100%; */
@@ -111,6 +110,7 @@ const Content = styled.div`
 
   object {
     border-radius: 10px;
+    min-height: 500px;
     width: 100%;
     height: 100%;
   }
